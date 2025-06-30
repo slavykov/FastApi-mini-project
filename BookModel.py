b8ac7bf5a1bd2969b6dae8543a5b1a03ebbfb708
slavykov <slavykpota2@gmail.com>
2025-06-30 15:19:02 +0300

    Добавлена поддержка SQLModel для работы с SQlite, добавлены методы lifespan, create_db_and_tables, get_session. Исправлены функции запросов app.get, app.post. Добавлена функция запроса app.delete.

--- BookModel.py
+++ BookModel.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-class Book_Model(BaseModel):
-    title: str
-    author: str
+
+class Book_Model(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(index=True)
+    author: str = Field(index=True)
--- main.py
+++ main.py
@@ -1,36 +1,62 @@
-from typing import Union, Any
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from typing import Union, Any, Annotated
+from fastapi import FastAPI, Depends, HTTPException, Query
+from sqlmodel import SQLModel, Session, select, create_engine
+
 from BookModel import Book_Model
 
-app = FastAPI()
 
-books = []
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
 
+connect_args = {"check_same_thread":False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/books",
          tags=["Книги"],
          summary="Получить все книги")
-def get_books():
+def get_books(session:SessionDep, offset: int=0, limit: Annotated[int, Query(le=100)]=100,)->list[Book_Model]:
+    books = session.exec(select(Book_Model).offset(offset).limit(limit)).all()
     return books
+
 
 
 @app.post("/books",
           tags=["Книги"],
           summary="Добавить книгу"
           )
-def add_book(Book: Book_Model):
-    books.append(
-        {"id": len(books) + 1,
-         "title": Book.title,
-         "author": Book.author
-         }
-    )
+def add_book(Book: Book_Model, session: SessionDep)->Book_Model:
+    session.add(Book)
+    session.commit()
+    session.refresh(Book)
     return {"success": True, "message": "Книга успешно добавлена"}
 
 
-@app.put("/books/{book_id}",
-        tags=["Книги"],
-        summary="Редактировать книгу"
-         )
-def update_item(book_id: int, Book: Book_Model):
-   return {"book_title": Book.title, "book_id": book_id}
+@app.delete("/books/{book_id}",
+            tags=["Книги"],
+            summary="Удалить книгу")
+def delete_book(book_id:int, session:SessionDep):
+    book = session.get(Book_Model, book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(book)
+    session.commit()
+    return {"ok":True}
